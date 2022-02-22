ccb8b0a4c3f918c3bf704933641af668e101cec2
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-02-22 14:17:55 -0500

    first commit

---
+++ src/main/java/com/stefwebdesigner/bankSpringBoot/BankSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.stefwebdesigner.bankSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSpringBootApplication.class, args);
+	}
+
+}
