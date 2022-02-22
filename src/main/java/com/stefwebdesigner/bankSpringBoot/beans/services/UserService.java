23193113f03b39c338bce99807704929af4e25cd
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-02-22 17:22:30 -0500

    get method added

--- src/main/java/com/stefwebdesigner/bankSpringBoot/beans/services/UserService.java
+++ src/main/java/com/stefwebdesigner/bankSpringBoot/beans/services/UserService.java
@@ -1,15 +1,19 @@
 package com.stefwebdesigner.bankSpringBoot.beans.services;
 
+import com.stefwebdesigner.bankSpringBoot.beans.entities.UserEntities;
 import com.stefwebdesigner.bankSpringBoot.beans.repositories.userRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class userService {
+public class UserService {
     private final userRepository userRepo;
 
     @Autowired
-    public userService(userRepository userRepo) {this.userRepo = userRepo;}
+    public UserService(userRepository userRepo) {this.userRepo = userRepo;}
 
 
+    public UserEntities getUserById(Integer userId) {
+        return userRepo.findById(userId).get();
+    }
 }
