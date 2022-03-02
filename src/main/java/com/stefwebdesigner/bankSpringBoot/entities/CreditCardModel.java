d8e1a8f1a418a765c78d57e0b513c8c012530865
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-03-02 11:43:38 -0500

    Added credit card model

--- src/main/java/com/stefwebdesigner/bankSpringBoot/entities/CreditCardModel.java
+++ src/main/java/com/stefwebdesigner/bankSpringBoot/entities/CreditCardModel.java
@@ -1,4 +1,50 @@
 package com.stefwebdesigner.bankSpringBoot.entities;
 
+import javax.persistence.*;
+import java.util.Iterator;
+
+@Entity
 public class CreditCardModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private Integer creditCardNumber;
+    private Integer limit;
+
+
+    @ManyToOne
+    BankAccountModel bankAccountModel;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(Integer creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
+
+    public BankAccountModel getBankAccountModel() {
+        return bankAccountModel;
+    }
+
+    public void setBankAccountModel(BankAccountModel bankAccountModel) {
+        this.bankAccountModel = bankAccountModel;
+    }
 }
